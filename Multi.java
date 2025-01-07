60e891b6fcd52ee58fbace6762c41e1603ef649f
2002prath <prathammishra1111@gmail.com>
2025-01-07 12:38:15 +0530

    (mulplication file is added)

---
+++ Multi.java
@@ -0,0 +1,15 @@
+import java.io.*; 
+import java.lang.*; 
+import java.util.*; 
+
+
+class Multi
+{ 
+  public static void main(String[] amg)
+   { 
+     int a = 14; 
+     int b = 7; 
+     int c = a*b; 
+     System.out.println(c);   
+   } 
+}
