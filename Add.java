fa61bd41a7315880f1cc1bf97496ce9b627747da
2002prath <prathammishra1111@gmail.com>
2025-01-07 11:15:07 +0530

    (sum and minus added)

---
+++ Add.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.lang.*; 
+import java.util.*; 
+
+
+
+class Add
+{ 
+  public static void main(String[] amg)
+   { 
+     Scanner sc = new Scanner(System.in); 
+         int sum=0; 
+         int  n1 = sc.nextInt(); 
+          int  n2 = sc.nextInt(); 
+         sum = n1 + n2; 
+         System.out.println(" addition "+ sum);
+          sc.close(); 
+   } 
+   
+ }    
---
+++ Minus.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.lang.*; 
+import java.util.Scanner; 
+
+class Minus 
+{ 
+  public static void main(String[] amg) 
+    { 
+      Scanner sc = new Scanner(System.in); 
+      int a = sc.nextInt(); 
+      int b = sc.nextInt(); 
+   
+      int m = a-b; 
+      System.out.println(m);  
+    }
+}
